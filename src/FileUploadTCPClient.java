4d78e626f19867a7a678a57bf5f170b87a051baf
unknown <940929347@qq.com>
2020-07-10 14:27:04 +0800

    上传文件 TCIPP

---
+++ src/FileUploadTCPClient.java
@@ -0,0 +1,13 @@
+package PACKAGE_NAME;
+
+/**
+ * Contact :   dzm_1995@163.com
+ * <p>
+ * Modify Time       Author     Version    Desciption
+ * ------------      -------    --------    -----------
+ * 2020/7/10 10:04   戴宗明       1.0         None
+ */
+
+
+public class FileUploadTCPClient {
+}
---
+++ src/FileUploadTCPServer.java
@@ -0,0 +1,11 @@
+/**
+ * Contact :   dzm_1995@163.com
+ * <p>
+ * Modify Time       Author     Version    Desciption
+ * ------------      -------    --------    -----------
+ * 2020/7/10 10:34   戴宗明       1.0         None
+ */
+
+
+public class FileUploadTCPServer {
+}
