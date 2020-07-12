cf6a8ed7a2ca76378d8bad9b0c31e8abe5f11df4
unknown <940929347@qq.com>
2020-07-12 14:32:48 +0800

    IDBC练习2

---
+++ src/JDBCPractice2.java
@@ -0,0 +1,46 @@
+import java.sql.*;
+
+/**
+ * Contact :   dzm_1995@163.com
+ * <p>
+ * Modify Time       Author     Version    Desciption
+ * ------------      -------    --------    -----------
+ * 2020/7/12 11:12   戴宗明       1.0         None
+ */
+
+
+public class JDBCPractice2 {
+    public static void main(String[] args){
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/selecttest?serverTimezone=GMT", "root", "dzm940929347");
+            statement = connection.createStatement();
+            String sql = "create table account (id int primary key, name varchar(20) not null);";
+            int count = statement.executeUpdate(sql);
+            System.out.println(count);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+    }
+}
