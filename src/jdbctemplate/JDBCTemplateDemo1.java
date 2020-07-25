009670e9499497d3934bea4442884ed6e812579a
unknown <940929347@qq.com>
2020-07-25 09:29:35 +0800

    JDBCTemplate 练习

---
+++ src/jdbctemplate/JDBCTemplateDemo1.java
@@ -0,0 +1,25 @@
+package jdbctemplate;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import utils.JDBCUtils;
+
+/**
+ * Contact :   dzm_1995@163.com
+ * <p>
+ * Modify Time       Author     Version    Desciption
+ * ------------      -------    --------    -----------
+ * 2020/7/24 21:49   戴宗明       1.0         None
+ */
+
+
+public class JDBCTemplateDemo1 {
+    public static void main(String[] args) {
+        // 1. 导入jar包
+        // 2 创建JDBCTemplate对象
+        JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
+        // 3 调用方法
+        String sql = "update account set balance = 5000 where id = ?;";
+        int count = template.update(sql, 1);
+        System.out.println(count);
+    }
+}
